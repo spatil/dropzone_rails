3874bba7e9834dccbc6b32458b4da09d2bee99ee
Shailesh Patil <shailesh@joshsoftware.com>
2013-07-30 17:41:16 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropzoneRails::Application.config.secret_token = 'e2808c0b9426bdf2eef812dc9022b3da59acd53ae981d7e7452c379c3c27421cbf854b7c45f45b3989d72b64c89133fcb28f6d4c2c52c6632baa4881a076e4e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DropzoneRails::Application.config.session_store :cookie_store, key: '_dropzone_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DropzoneRails::Application.config.session_store :active_record_store
